9ea955f026fc064efed1a4aae69973508052d7bc
jordankanghm <jordankanghm@gmail.com>
2024-05-19 21:42:25 +0800

    Debug the DataLoaders
    Currently, the way the data is being preprocessed results in the images and labels in the dataloaders
    to be misaligned.
    
    This results in the model learning based on incorrect data, which results in inaccurate predictions.
    
    In the custom dataset class, the images are sorted to ensure that the images align with the labels.
    This corroborates with the assumption that the data was also extracted in such an order.
    
    Ensuring that the images in the data loader are ordered in the same manner as those in the
    directories storing the images ensures that the images and labels are aligned correctly.

--- image-classification/utils/data_preprocessing.py
+++ image-classification/utils/data_preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
+import matplotlib.pyplot as plt
 
 # Define paths
 TRAIN_IMAGES_DIR = '../datasets/train-images'
@@ -14,7 +15,8 @@ class ImageDataset(Dataset):
         self.images_dir = images_dir
         self.labels = labels
         self.transform = transform
-        self.image_files = sorted([f for f in os.listdir(images_dir) if f.endswith('.png')])
+        # Sort the images to ensure images and labels are aligned
+        self.image_files = sorted([f for f in os.listdir(images_dir)])
 
     def __len__(self):
         return len(self.image_files)
@@ -32,7 +34,7 @@ class ImageDataset(Dataset):
 # Load labels
 labels = np.load(TRAIN_LABELS_PATH)
 
-# Define a transform for the images
+# Normalise the images before feeding to the model
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
@@ -49,3 +51,28 @@ train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 # Create DataLoaders for the training and test sets
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+"""
+Function to check whether the images and labels in the data loader are correct
+"""
+# def visualize_batch(data_loader, num_images=10):
+#     data_iter = iter(data_loader)
+#     images, labels = next(data_iter)
+#     images = images.numpy()
+#     labels = labels.numpy()
+#
+#     plt.figure(figsize=(15, 10))
+#     for i in range(num_images):
+#         plt.subplot(2, 5, i + 1)
+#         img = images[i].transpose((1, 2, 0))  # Convert from CHW to HWC format
+#         img = img * 0.5 + 0.5  # Unnormalize
+#         plt.imshow(img, cmap='gray')
+#         plt.title(f"Label: {labels[i]}")
+#         plt.axis('off')
+#     plt.show()
+#
+# # Visualize the first 10 training images with labels
+# visualize_batch(train_loader, num_images=10)
+#
+# # Visualize the first 10 test images with labels
+# visualize_batch(test_loader, num_images=10)
